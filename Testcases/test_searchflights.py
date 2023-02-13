3b1b9a495702d007f3fb2d07f1e9961ad7734e2f
azatbayram <bayramov.azat.tkm@gmail.com>
2023-02-13 18:15:05 +0300

    automation framework is created

--- Testcases/test_searchflights.py
+++ Testcases/test_searchflights.py
@@ -1,8 +1,16 @@
 import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
 
 
 @pytest.mark.usefixtures("setup")
 class TestSearchAndVerifyFilter():
 
     def test_search_flights(self):
+
+        launchPage=LaunchPage(self.driver, self.wait)
+
+
 
